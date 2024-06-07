565c76f899ceaf9484d47aac930e6e8c1c7533d6
Saurav Raj <113482311+rajsaurav4055@users.noreply.github.com>
2024-06-06 20:40:09 -0400

    Added Event handlers

--- src/base/classes/CustomClient.ts
+++ src/base/classes/CustomClient.ts
@@ -1,18 +1,25 @@
 import { Client } from "discord.js";
 import ICustomClient from "../interfaces/iCustomClient";
 import IConfig from "../interfaces/IConfig";
+import Handler from "./Handler";
 
 export default class CustomClient extends Client implements ICustomClient{
-    
+    handler: Handler;
     config: IConfig;
 
     constructor() {
         super({ intents: []});
         this.config= require(`${process.cwd()}/data/config.json`);
+        this.handler= new Handler(this);
     }
+    
     Init(): void {
+        this.LoadHandlers();
         this.login(this.config.token)
-        .then(() => console.log("Logged in!"))
-        .catch((err)=> console.error(err))
+            .catch((err)=> console.error(err))
+    }
+
+    LoadHandlers(): void {
+        this.handler.LoadEvents();
     }
 }    
